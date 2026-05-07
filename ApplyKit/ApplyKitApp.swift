d9bbde2ce046cea9a64eeac34c0ff488f801852c
Leonard Chen <wxclx98@gmail.com>
2026-05-07 01:47:15 -0400

    Initial Commit

---
+++ ApplyKit/ApplyKitApp.swift
@@ -0,0 +1,32 @@
+//
+//  ApplyKitApp.swift
+//  ApplyKit
+//
+//  Created by Leonard Chen on 5/7/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ApplyKitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
